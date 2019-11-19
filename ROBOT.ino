75586f0dc2bef2bc2f4aba78f8c44d65f788e0b6
rggone <36920706+rggone@users.noreply.github.com>
2019-11-19 21:43:10 +0800

    Add files via upload

---
+++ ROBOT.ino
@@ -0,0 +1,314 @@
+/*        
+       DIY Arduino Robot Arm Smartphone Control   
+*/
+
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+Servo servo01;
+Servo servo02;
+Servo servo03;
+Servo servo04;
+Servo servo05;
+Servo servo06;
+
+SoftwareSerial Bluetooth(3, 4); // Arduino(RX, TX) - HC-05 Bluetooth (TX, RX)
+
+int servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos; // current position
+int servo1PPos, servo2PPos, servo3PPos, servo4PPos, servo5PPos, servo6PPos; // previous 位置
+int servo01SP[50], servo02SP[50], servo03SP[50], servo04SP[50], servo05SP[50], servo06SP[50]; // 用于存储位置/步骤
+int speedDelay = 20;
+int index = 0;
+String dataIn = "";
+
+void setup() {
+  servo01.attach(5);
+  servo02.attach(6);
+  servo03.attach(7);
+  servo04.attach(8);
+  servo05.attach(9);
+  servo06.attach(10);
+  Bluetooth.begin(38400); // 蓝牙模块的默认波特率
+  Bluetooth.setTimeout(1);
+  delay(20);
+  // 机械手初始位置
+  servo1PPos = 90;
+  servo01.write(servo1PPos);
+  servo2PPos = 150;
+  servo02.write(servo2PPos);
+  servo3PPos = 35;
+  servo03.write(servo3PPos);
+  servo4PPos = 140;
+  servo04.write(servo4PPos);
+  servo5PPos = 85;
+  servo05.write(servo5PPos);
+  servo6PPos = 80;
+  servo06.write(servo6PPos);
+}
+
+void loop() {
+  // 检查输入数据
+  if (Bluetooth.available() > 0) {
+    dataIn = Bluetooth.readString();  // 将数据读取为字符串
+    
+    // 如果“腰”滑块改变了值-移动伺服1到位置
+    if (dataIn.startsWith("s1")) {
+      String dataInS = dataIn.substring(2, dataIn.length()); // 只提取数字。例如:从“s1120”到“120”
+      servo1Pos = dataInS.toInt();  // 将字符串转换为整数
+      // 我们使用的循环，所以我们可以控制速度的伺服
+      // 如果以前的位置比现在的位置大
+      if (servo1PPos > servo1Pos) {
+        for ( int j = servo1PPos; j >= servo1Pos; j--) {   // 伺服运行下
+          servo01.write(j);
+          delay(20);    // 定义伺服旋转的速度
+        }
+      }
+      // 如果以前的职位比现在的小
+      if (servo1PPos < servo1Pos) {
+        for ( int j = servo1PPos; j <= servo1Pos; j++) {   // 伺服运行起来
+          servo01.write(j);
+          delay(20);
+        }
+      }
+      servo1PPos = servo1Pos;   // 将当前位置设置为以前的位置
+    }
+    
+    // Move Servo 2
+    if (dataIn.startsWith("s2")) {
+      String dataInS = dataIn.substring(2, dataIn.length());
+      servo2Pos = dataInS.toInt();
+
+      if (servo2PPos > servo2Pos) {
+        for ( int j = servo2PPos; j >= servo2Pos; j--) {
+          servo02.write(j);
+          delay(50);
+        }
+      }
+      if (servo2PPos < servo2Pos) {
+        for ( int j = servo2PPos; j <= servo2Pos; j++) {
+          servo02.write(j);
+          delay(50);
+        }
+      }
+      servo2PPos = servo2Pos;
+    }
+    // Move Servo 3
+    if (dataIn.startsWith("s3")) {
+      String dataInS = dataIn.substring(2, dataIn.length());
+      servo3Pos = dataInS.toInt();
+      if (servo3PPos > servo3Pos) {
+        for ( int j = servo3PPos; j >= servo3Pos; j--) {
+          servo03.write(j);
+          delay(30);
+        }
+      }
+      if (servo3PPos < servo3Pos) {
+        for ( int j = servo3PPos; j <= servo3Pos; j++) {
+          servo03.write(j);
+          delay(30);
+        }
+      }
+      servo3PPos = servo3Pos;
+    }
+    // Move Servo 4
+    if (dataIn.startsWith("s4")) {
+      String dataInS = dataIn.substring(2, dataIn.length());
+      servo4Pos = dataInS.toInt();
+      if (servo4PPos > servo4Pos) {
+        for ( int j = servo4PPos; j >= servo4Pos; j--) {
+          servo04.write(j);
+          delay(30);
+        }
+      }
+      if (servo4PPos < servo4Pos) {
+        for ( int j = servo4PPos; j <= servo4Pos; j++) {
+          servo04.write(j);
+          delay(30);
+        }
+      }
+      servo4PPos = servo4Pos;
+    }
+    // Move Servo 5
+    if (dataIn.startsWith("s5")) {
+      String dataInS = dataIn.substring(2, dataIn.length());
+      servo5Pos = dataInS.toInt();
+      if (servo5PPos > servo5Pos) {
+        for ( int j = servo5PPos; j >= servo5Pos; j--) {
+          servo05.write(j);
+          delay(30);
+        }
+      }
+      if (servo5PPos < servo5Pos) {
+        for ( int j = servo5PPos; j <= servo5Pos; j++) {
+          servo05.write(j);
+          delay(30);
+        }
+      }
+      servo5PPos = servo5Pos;
+    }
+    // Move Servo 6
+    if (dataIn.startsWith("s6")) {
+      String dataInS = dataIn.substring(2, dataIn.length());
+      servo6Pos = dataInS.toInt();
+      if (servo6PPos > servo6Pos) {
+        for ( int j = servo6PPos; j >= servo6Pos; j--) {
+          servo06.write(j);
+          delay(30);
+        }
+      }
+      if (servo6PPos < servo6Pos) {
+        for ( int j = servo6PPos; j <= servo6Pos; j++) {
+          servo06.write(j);
+          delay(30);
+        }
+      }
+      servo6PPos = servo6Pos; 
+    }
+    // 如果按下“保存”按钮
+    if (dataIn.startsWith("SAVE")) {
+      servo01SP[index] = servo1PPos;  // 保存位置到数组中
+      servo02SP[index] = servo2PPos;
+      servo03SP[index] = servo3PPos;
+      servo04SP[index] = servo4PPos;
+      servo05SP[index] = servo5PPos;
+      servo06SP[index] = servo6PPos;
+      index++;                        // 增加数组索引
+    }
+    // 如果按下“运行”按钮
+    if (dataIn.startsWith("RUN")) {
+      runservo();  // 自动模式-运行保存的步骤 
+    }
+    // 如果按下按钮“复位”
+    if ( dataIn == "RESET") {
+      memset(servo01SP, 0, sizeof(servo01SP)); // 将数组数据清除为0
+      memset(servo02SP, 0, sizeof(servo02SP));
+      memset(servo03SP, 0, sizeof(servo03SP));
+      memset(servo04SP, 0, sizeof(servo04SP));
+      memset(servo05SP, 0, sizeof(servo05SP));
+      memset(servo06SP, 0, sizeof(servo06SP));
+      index = 0;  // Index to 0
+    }
+  }
+}
+
+// 自动模式自定义功能-运行保存的步骤
+void runservo() {
+  while (dataIn != "RESET") {   // 反复运行这些步骤，直到按下“复位”按钮
+    for (int i = 0; i <= index - 2; i++) {  // 遍历所有步骤(索引)
+      if (Bluetooth.available() > 0) {      // 检查输入数据
+        dataIn = Bluetooth.readString();
+        if ( dataIn == "PAUSE") {           // 如果按下按钮“暂停”
+          while (dataIn != "RUN") {         // 等待，直到“运行”再次按下
+            if (Bluetooth.available() > 0) {
+              dataIn = Bluetooth.readString();
+              if ( dataIn == "RESET") {     
+                break;
+              }
+            }
+          }
+        }
+        // 如果速度滑块改变
+        if (dataIn.startsWith("ss")) {
+          String dataInS = dataIn.substring(2, dataIn.length());
+          speedDelay = dataInS.toInt(); // 改变伺服速度(延时时间)
+        }
+      }
+      // Servo 1
+      if (servo01SP[i] == servo01SP[i + 1]) {
+      }
+      if (servo01SP[i] > servo01SP[i + 1]) {
+        for ( int j = servo01SP[i]; j >= servo01SP[i + 1]; j--) {
+          servo01.write(j);
+          delay(speedDelay);
+        }
+      }
+      if (servo01SP[i] < servo01SP[i + 1]) {
+        for ( int j = servo01SP[i]; j <= servo01SP[i + 1]; j++) {
+          servo01.write(j);
+          delay(speedDelay);
+        }
+      }
+
+      // Servo 2
+      if (servo02SP[i] == servo02SP[i + 1]) {
+      }
+      if (servo02SP[i] > servo02SP[i + 1]) {
+        for ( int j = servo02SP[i]; j >= servo02SP[i + 1]; j--) {
+          servo02.write(j);
+          delay(speedDelay);
+        }
+      }
+      if (servo02SP[i] < servo02SP[i + 1]) {
+        for ( int j = servo02SP[i]; j <= servo02SP[i + 1]; j++) {
+          servo02.write(j);
+          delay(speedDelay);
+        }
+      }
+
+      // Servo 3
+      if (servo03SP[i] == servo03SP[i + 1]) {
+      }
+      if (servo03SP[i] > servo03SP[i + 1]) {
+        for ( int j = servo03SP[i]; j >= servo03SP[i + 1]; j--) {
+          servo03.write(j);
+          delay(speedDelay);
+        }
+      }
+      if (servo03SP[i] < servo03SP[i + 1]) {
+        for ( int j = servo03SP[i]; j <= servo03SP[i + 1]; j++) {
+          servo03.write(j);
+          delay(speedDelay);
+        }
+      }
+
+      // Servo 4
+      if (servo04SP[i] == servo04SP[i + 1]) {
+      }
+      if (servo04SP[i] > servo04SP[i + 1]) {
+        for ( int j = servo04SP[i]; j >= servo04SP[i + 1]; j--) {
+          servo04.write(j);
+          delay(speedDelay);
+        }
+      }
+      if (servo04SP[i] < servo04SP[i + 1]) {
+        for ( int j = servo04SP[i]; j <= servo04SP[i + 1]; j++) {
+          servo04.write(j);
+          delay(speedDelay);
+        }
+      }
+
+      // Servo 5
+      if (servo05SP[i] == servo05SP[i + 1]) {
+      }
+      if (servo05SP[i] > servo05SP[i + 1]) {
+        for ( int j = servo05SP[i]; j >= servo05SP[i + 1]; j--) {
+          servo05.write(j);
+          delay(speedDelay);
+        }
+      }
+      if (servo05SP[i] < servo05SP[i + 1]) {
+        for ( int j = servo05SP[i]; j <= servo05SP[i + 1]; j++) {
+          servo05.write(j);
+          delay(speedDelay);
+        }
+      }
+
+      // Servo 6
+      if (servo06SP[i] == servo06SP[i + 1]) {
+      }
+      if (servo06SP[i] > servo06SP[i + 1]) {
+        for ( int j = servo06SP[i]; j >= servo06SP[i + 1]; j--) {
+          servo06.write(j);
+          delay(speedDelay);
+        }
+      }
+      if (servo06SP[i] < servo06SP[i + 1]) {
+        for ( int j = servo06SP[i]; j <= servo06SP[i + 1]; j++) {
+          servo06.write(j);
+          delay(speedDelay);
+        }
+      }
+    }
+  }
+  Serial.println(Bluetooth);
+}
